db47f90cd4249cfd154d0f3a79ed6e710b841050
Alexander Zhirov <aleks@aradmin.org.ru>
2011-04-18 22:46:04 +0700

    Introduced view table column model for handling column data in JTable

--- UniTable/src/com/nevkontakte/unitable/view/UnitableViewModel.java
+++ UniTable/src/com/nevkontakte/unitable/view/UnitableViewModel.java
@@ -7,7 +7,7 @@ import com.nevkontakte.unitable.model.UnitableRowSet;
 
 import javax.swing.table.AbstractTableModel;
 import java.sql.SQLException;
-import java.util.Iterator;
+import java.util.ArrayList;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,9 +19,18 @@ import java.util.Iterator;
  */
 public class UnitableViewModel extends AbstractTableModel {
 	protected final TableData tableData;
+	protected final ArrayList<ViewColumnModel> columns = new ArrayList<ViewColumnModel>();
 
 	public UnitableViewModel(TableData tableData) {
 		this.tableData = tableData;
+
+		int i = 0;
+		for(ColumnModel column : this.tableData.getTableModel().getColumns().values()) {
+			if(!column.isHidden()) {
+				this.columns.add(new DataViewColumnModel(i, column, this.tableData));
+			}
+			i++;
+		}
 	}
 
 	public int getRowCount() {
@@ -35,65 +44,96 @@ public class UnitableViewModel extends AbstractTableModel {
 	}
 
 	public int getColumnCount() {
-		return this.tableData.getTableModel().getColumns().size();
+		return this.columns.size();
 	}
 
 	public Object getValueAt(int rowIndex, int columnIndex) {
-		try {
-			UnitableRowSet data = this.tableData.getTableContents(false);
-			data.absolute(rowIndex + 1);
-			return data.getObject(columnIndex + 1);
-		} catch (SQLException e) {
-			// TODO: handle nicely
-			e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-			return null;
-		}
+		return this.columns.get(columnIndex).getValueAt(rowIndex);
 	}
 
 	@Override
 	public void setValueAt(Object aValue, int rowIndex, int columnIndex) {
-		try {
-			UnitableRowSet rows = this.tableData.getTableContents(false);
-			rows.absolute(rowIndex + 1);
-			rows.updateObject(columnIndex + 1, aValue);
-			rows.updateRow();
-			System.out.println(aValue);
-		} catch (SQLException e) {
-			// TODO: Report error
-			e.printStackTrace();
-		}
+		this.columns.get(columnIndex).setValueAt(aValue, rowIndex);
 	}
 
 	@Override
 	public String getColumnName(int column) {
-		Iterator<ColumnModel> i = this.tableData.getTableModel().getColumns().values().iterator();
-		ColumnModel columnModel = null;
-		for (int j = 0; j <= column; j++) {
-			columnModel = i.next();
-		}
-		return columnModel.getName();
+		return this.columns.get(column).getColumnName();
 	}
 
 	@Override
 	public Class<?> getColumnClass(int columnIndex) {
-		String columnName = this.getColumnName(columnIndex);
-		ColumnModel columnModel = this.tableData.getTableModel().getColumns().get(columnName);
-		int columnType = columnModel.getType();
-		Class<?> cls = JdbcTypeHelper.getClassByInt(columnType);
-		if (cls == null) {
-			cls = String.class;
-		}
-		return cls;
+		return this.columns.get(columnIndex).getColumnClass();
 	}
 
 	@Override
 	public boolean isCellEditable(int rowIndex, int columnIndex) {
-		String columnName = this.getColumnName(columnIndex);
-		ColumnModel columnModel = this.tableData.getTableModel().getColumns().get(columnName);
-		return !this.tableData.getTableModel().getPrimaryKeys().contains(columnModel);
+		return this.columns.get(columnIndex).isCellEditable(rowIndex);
 	}
 
 	public TableData getTableData() {
 		return tableData;
+	}
+
+	private static interface ViewColumnModel {
+		public String getColumnName();
+		public Class<?> getColumnClass();
+		public Object getValueAt(int rowIndex);
+		public void setValueAt(Object aValue, int rowIndex);
+		public boolean isCellEditable(int rowIndex);
+	}
+
+	private static class DataViewColumnModel implements ViewColumnModel {
+		final int dataColumnIndex;
+		final ColumnModel columnModel;
+		final TableData tableData;
+
+		public DataViewColumnModel(int dataColumnIndex, ColumnModel columnModel, TableData tableData) {
+			this.dataColumnIndex = dataColumnIndex;
+			this.columnModel = columnModel;
+			this.tableData = tableData;
+		}
+
+
+		public String getColumnName() {
+			return columnModel.getName();
+		}
+
+		public Class<?> getColumnClass() {
+			int columnType = columnModel.getType();
+			Class<?> cls = JdbcTypeHelper.getClassByInt(columnType);
+			if (cls == null) {
+				cls = String.class;
+			}
+			return cls;
+		}
+
+		public Object getValueAt(int rowIndex) {
+			try {
+				UnitableRowSet data = this.tableData.getTableContents(false);
+				data.absolute(rowIndex + 1);
+				return data.getObject(dataColumnIndex + 1);
+			} catch (SQLException e) {
+				// TODO: handle nicely
+				e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+				return null;
+			}
+		}
+
+		public void setValueAt(Object aValue, int rowIndex) {
+			try {
+				UnitableRowSet rows = this.tableData.getTableContents(false);
+				rows.absolute(rowIndex + 1);
+				rows.updateObject(this.dataColumnIndex + 1, aValue);
+				rows.updateRow();
+			} catch (SQLException e) {
+				// TODO: Report error
+				e.printStackTrace();
+			}
+		}
+
+		public boolean isCellEditable(int rowIndex) {
+			return true;
+		}
 	}
 }
